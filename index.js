24d3d87673ff0dc0010dfd0a158f7db6f357f5d9
mdarifali <arif.raj2050@gmail.com>
2022-05-25 11:33:15 +0600

    user data insert

--- index.js
+++ index.js
@@ -23,13 +23,27 @@ async function run (){
         await client.connect();
         const productsCollection = client.db('car-parts-manufacturers').collection('products');
         const ordersCollection = client.db('car-parts-manufacturers').collection('orders');
+        const userCollection = client.db('car-parts-manufacturers').collection('user');
 
         // Get Product Api data //
-        app.get('/products', async(req, res) => {
+        app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productsCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        // User singup insert Api data //
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email : email};
+            const options = {upsert: true};
+            const updatedData ={
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updatedData, options);
+            res.send(result);
         })
 
         // Get to Find Api id //
@@ -41,11 +55,21 @@ async function run (){
             res.send(result);
         });
 
+        // Insert order api //
         app.post('/orders', async (req, res) => {
             const orders = req.body;
             const result = await ordersCollection.insertOne(orders);
             return res.send({success: true, result});
-        })
+        });
+
+
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email; 
+            const query = {  email: email };
+            const cursor = ordersCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
     }
     finally{
